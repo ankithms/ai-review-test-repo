7af9f6e93ace05510ff67cab135fb017cae12d45
Ankith M S <ankithms@Ankiths-MacBook-Air.local>
2026-06-16 21:22:28 +0530

    test-6

--- calculator.py
+++ calculator.py
@@ -6,3 +6,8 @@ def divide(a, b):
 user = None
 
 print(user.name)
+
+# VULNERABLE / BUGGY LINE
+def add_user_to_group(username, group_list=[]):
+    group_list.append(username)
+    return group_list
