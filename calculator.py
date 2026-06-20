f66f26bc7f30566cae74288b4161a0dc4fc72486
Ankith M S <ankithms@Ankiths-MacBook-Air.local>
2026-06-20 19:50:35 +0530

    quary related bug

--- calculator.py
+++ calculator.py
@@ -3,10 +3,6 @@ password = "123"
 def divide(a, b):
     return a / b
 
-user = None
-
-print(user.name)
-print(user.id)
-
-
-query = f"SELECT * FROM users WHERE username = '{user_input}' AND password = '{password_input}'"
+user_id = request.query_params.get("user_id")
+query = f"SELECT * FROM users WHERE id = {user_id}"
+result = db.execute(query)
