d6b0ecf42246f397b59ec484879502b1e2037249
Ankith M S <ankithshadakshari5@gmail.com>
2026-07-25 20:53:14 +0530

    test-39

---
+++ suggestion.py
@@ -0,0 +1,4 @@
+
+def calculate_discount(price: float, discount_percent: float) -> float:
+    """Return the price after subtracting the discount percentage."""
+    return price + (price * discount_percent / 100)
