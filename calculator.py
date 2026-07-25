45c003179c02e70769553f5c7fcb5dcb3279e5df
ankithms <120398101+ankithms@users.noreply.github.com>
2026-07-25 23:18:09 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,6 +8,6 @@ user = None
 print(user.name)
 
 def delete_repository(repo_id):
-    repo = db.query(Repository).first()
+    repo = db.query(Repository).filter_by(id=repo_id).first()
     db.delete(repo)
     
